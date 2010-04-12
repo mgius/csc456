77a80a86572139ae79d6a75202ba41ff8eefed90
Mark Gius <mgius7096@gmail.com>
2010-04-12 15:54:33 -0700

    Initial checking of program1 in perl.  Cause I'm stubborn like that

---
+++ program1/program1.pl
@@ -0,0 +1,53 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Std;
+
+# copied from spec
+sub _usage() {
+   print "usage: $1 [ -v] [ -d] key [infile [ outfile ] ]\n";
+   exit 1;
+}
+
+my %opts;
+getopts('vs:p:', \%opts);
+
+my $verbose = exists($opts{v}); # currently does nothing
+my $skip = exists($opts{s}) ? $opts{s} : 0;
+my $period = exists($opts{p}) ? $opts{p} : 0;
+
+# validate options
+$skip =~ m/^\d+$/ or print "Invalid skip value \"$skip\"\n" and exit 1;
+$period =~ m/^\d+$/ or print "Invalid period value \"$period\"\n" and exit 1;
+
+if (@ARGV < 1) {
+   _usage();
+}
+
+my $key = shift;
+
+# toUpper and verify alpha only
+$key =~ tr/[a-z]/[A-Z]/;
+$key =~ m/([^A-Z]+)/ 
+   and print "key must be alphabetic only. String $1 not valid\n" and exit 1;
+
+# pull filenames for in and out, default use STDIN/STDOUT
+my $inHandle = *STDIN;
+if (@ARGV > 0) {
+   my $inFilename = shift;
+   open($inHandle, '<', "$inFilename") or die $!;
+}
+
+my $outHandle = *STDOUT;
+
+if (@ARGV > 0) {
+   my $filename = shift;
+   open($outHandle, '>', "$filename") or die $!;
+}
+
+my @lines = <$inHandle>;
+
+# chr = num -> char
+# ord = char -> num
+
+$outHandle eq *STDOUT or close($outHandle);
