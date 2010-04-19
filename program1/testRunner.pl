e677e296d287e0638a0cb1dd9a553e2e57609403
Mark Gius <mgius7096@gmail.com>
2010-04-19 11:20:50 -0700

    Reworked the testing infrsructure to be more elegant

---
+++ program1/testRunner.pl
@@ -0,0 +1,17 @@
+$prog = "perl vig.pl";
+$testcase = shift;
+
+open(TESTCASE, '<', "$testcase") or die $!;
+
+@testArray = <TESTCASE>;
+$input = shift @testArray;
+
+while(@testArray) {
+   $args = shift @testArray;
+   $expectedOutput = shift @testArray;
+   $output = `echo -n "$input" | $prog $args`;
+   if ($expectedOutput ne $output) {
+      print "\"$expectedOutput\" does not match \"$output\"" and exit 1;
+   }
+   $input = $output;
+}
