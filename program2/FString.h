5c54b6122a148134ac44925c579b1caffe6bd0df
Mark Gius <mgius@simpson.off.sbp1.createspace.com>
2010-05-17 00:34:01 -0700

    ADding a helper class from 305

---
+++ program2/FString.h
@@ -0,0 +1,21 @@
+/* this is something that was given to me in 305
+	Basically, it allows you to create c++ string objects
+	that are formatted using printf style rules
+ */
+#include <stdarg.h>
+#include <string>
+#include <stdio.h>
+class FString : public std::string {
+	public:
+		FString(char *fmt, ...) {
+			static const int bufLen = 1024;
+			static char buf[bufLen];
+			va_list args;
+
+			va_start(args, fmt); 
+			vsnprintf(buf, bufLen, fmt, args);
+			va_end(args);
+
+			*(std::string *)this = std::string(buf);
+		}
+};
