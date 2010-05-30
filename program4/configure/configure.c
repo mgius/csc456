c27ff81745fe4b258d348c2448e44ac681bd81b2
Mark <mgius7096@gmail.com>
2010-05-30 01:33:36 -0700

    I think this is better, but I can't reproduce on my laptop

--- program4/configure/configure.c
+++ program4/configure/configure.c
@@ -1,68 +1,30 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-#define STDOUT_NO 1
-#define FIRST 0x10000000
-#define SECOND 0x01000000
-#define THIRD 0x00100000
-#define FOURTH 0x00010000
-#define FIFTH 0x00001000
-#define SIXTH 0x00000100
-#define SEVENTH 0x00000010
-#define EIGHTH 0x00000001
-
-int convertHex (char c) {
-	if ((c == 'A')||(c == 'a')) {
-		return 10;
-	}
-	else if ((c == 'B') ||(c=='b')) {
-		return 11;
-	}
-	else if ((c == 'C')||(c=='c')) {
-		return 12;
-	}
-	else if ((c == 'D')||(c == 'd')) {
-		return 13;
-	}
-	else if ((c == 'E')||(c == 'e')) {
-		return 14;
-	}
-	else if ((c == 'F')||(c == 'f')) {
-		return 15;
-	}
-	else return (c - 48);
-}
+#include <unistd.h>
 
 int main(int argc, const char* argv[]) {
-	int writeCount, uncalledAddress;
-
-
+	unsigned int writeCount, uncalledAddress;
 	char standard[12] = "abcdabcdabcd";
-	uncalledAddress = FIRST * convertHex(argv[2][2]);
-	uncalledAddress += SECOND * convertHex(argv[2][3]);
-	uncalledAddress += THIRD * convertHex(argv[2][4]);
-	uncalledAddress += FOURTH * convertHex(argv[2][5]);
-	uncalledAddress += FIFTH * convertHex(argv[2][6]);
-	uncalledAddress += SIXTH * convertHex(argv[2][7]);
-	uncalledAddress += SEVENTH * convertHex(argv[2][8]);
-	uncalledAddress += EIGHTH * convertHex(argv[2][9]);
-	/*fprintf(stderr, "Address entered: %x\n", uncalledAddress);*/
-	writeCount = write(STDOUT_NO, standard,12);
-	if (writeCount != 12) {
-		fprintf(stderr,"ERROR: Expected to prime buffer with 12 characters, wrote %d\n", writeCount);
+
+   if (argc < 3) {
+      perror("Usage: configure BaseAsHex UncalledAsHex");
+      exit(1);
+   }
+   if (1 != sscanf(argv[2], "%X", &uncalledAddress)) {
+      perror("Couldn't scan second argument as hexadecimal");
+      exit(1);
+   }
+	if (12 != (writeCount = write(STDOUT_FILENO, standard,12))) {
+		error(1, errno, 
+            "ERROR: Expected to prime buffer with 12 characters, wrote %d\n", 
+            writeCount);
 	}
-	if (writeCount == -1) {
-		fprintf(stderr, "Error writing: %s\n", strerror(errno));
-	}
-	writeCount = write(STDOUT_NO, &uncalledAddress, 4);
-	if (writeCount != 4) {
-		fprintf(stderr, "ERROR: Expected to write address as 4 bytes, wrote %d\n", writeCount);
-	}
-	if (writeCount == -1) {
-		fprintf(stderr, "Error writing: %s\n", strerror(errno));
-	}
-									
+	if (4 != (writeCount = write(STDOUT_FILENO, &uncalledAddress, 4))) {
+		error(1, errno, 
+		      "ERROR: Expected to write address as 4 bytes, wrote %d\n", 
+            writeCount);
+   }
 }
